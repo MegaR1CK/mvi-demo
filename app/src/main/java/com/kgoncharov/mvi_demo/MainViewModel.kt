b62dc5b4e53230af26b3ca1942a3add51f4b6a64
Konstantin Goncharov <kostjag15@gmail.com>
2022-06-18 17:19:57 +0300

    Init commit

---
+++ app/src/main/java/com/kgoncharov/mvi_demo/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.kgoncharov.mvi_demo
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor() : ViewModel() {
+}
