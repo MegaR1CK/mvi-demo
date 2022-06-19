408dd464be2a74f3730cc3cb7685ca63dbd9c1b2
Konstantin Goncharov <kostjag15@gmail.com>
2022-06-19 23:15:34 +0300

    Add UI layer

---
+++ app/src/main/java/com/kgoncharov/mvi_demo/presentation/main/taskslist/TasksAdapter.kt
@@ -0,0 +1,33 @@
+package com.kgoncharov.mvi_demo.presentation.main.taskslist
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.kgoncharov.mvi_demo.data.Task
+import com.kgoncharov.mvi_demo.databinding.ItemTaskBinding
+import javax.inject.Inject
+
+class TasksAdapter @Inject constructor() : RecyclerView.Adapter<TaskViewHolder>() {
+
+    private val items = mutableListOf<Task>()
+
+    var onCheckedChanged: (Int, Boolean) -> Unit = { _, _ -> }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        return TaskViewHolder(ItemTaskBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        holder.bind(items[position], onCheckedChanged)
+    }
+
+    override fun getItemCount() = items.size
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun submitList(items: List<Task>) {
+        this.items.clear()
+        this.items.addAll(items)
+        notifyDataSetChanged()
+    }
+}
